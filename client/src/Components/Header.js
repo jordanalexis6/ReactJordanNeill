30a266fbbcb42d0b9b2d72ed077b45fb92690edf
Jordan <jordan.a.neill@gmail.com>
2020-09-04 11:05:56 -0500

    changing background

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -4,11 +4,8 @@ import ParticlesBg from "particles-bg";
 class Header extends Component {
   render() {
     if (this.props.data) {
-      //   var project = this.props.data.project;
-      //   var github = this.props.data.github;
       var name = this.props.data.name;
       var description = this.props.data.description;
-      // var city = this.props.data.address.city;
       var networks = this.props.data.social.map(function (network) {
         return (
           <li key={network.name}>
@@ -19,10 +16,41 @@ class Header extends Component {
         );
       });
     }
+    let config = {
+      num: [4, 7],
+      rps: 0.1,
+      radius: [5, 40],
+      life: [1.5, 3],
+      v: [2, 3],
+      tha: [-40, 40],
+      alpha: [0.6, 0],
+      scale: [0.1, 0.4],
+      position: "all",
+      color: ["random", "#ff0000"],
+      cross: "dead",
+      // emitter: "follow",
+      random: 15,
+    };
 
+    if (Math.random() > 0.85) {
+      config = Object.assign(config, {
+        onParticleUpdate: (ctx, particle) => {
+          ctx.beginPath();
+          ctx.rect(
+            particle.p.x,
+            particle.p.y,
+            particle.radius * 2,
+            particle.radius * 2
+          );
+          ctx.fillStyle = particle.color;
+          ctx.fill();
+          ctx.closePath();
+        },
+      });
+    }
     return (
       <header id="home">
-        <ParticlesBg type="circle" bg={true} />
+        <ParticlesBg type="custom" config={config} bg={true} />
         <nav id="nav-wrap">
           <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
             Show navigation
